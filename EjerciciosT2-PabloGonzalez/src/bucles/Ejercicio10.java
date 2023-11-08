df4d6bee5a390fc7964c5780f09af7f1f551e8ba
jpgonzalez <jpgonzalez@107-07.info2>
2023-11-08 10:52:31 +0100

    Completado --> 9

---
+++ EjerciciosT2-PabloGonzalez/src/bucles/Ejercicio10.java
@@ -0,0 +1,11 @@
+package bucles;
+
+public class Ejercicio10 {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
--- EjerciciosT2-PabloGonzalez/src/bucles/Ejercicio9.java
+++ EjerciciosT2-PabloGonzalez/src/bucles/Ejercicio9.java
@@ -1,21 +1,40 @@
 package bucles;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ejercicio9 {
 
 	public static void main(String[] args) {
 		
+		// Abrimos el scanner
 		Scanner sc = new Scanner(System.in);
 		
+		// Variable donde guardamos el número que introduzca el usuario
 		int num;
 		
-		try {
+		// Variable donde se guardará el número de cifras que tiene el número
+		int cont = 0;
+		
+		// Le pedimos al usuario un número mayor a 0
+		System.out.println("Dime un número mayor a 0");
+		num = sc.nextInt();
+		
+		// Comprobamos que el número sea mayor a 0
+		if(num > 0) {
+			// Con el bucle vamos mirando dígito a dígito hasta que i sea mayor que num y ya sabremos cuantos dígitos tiene mediante el cont
+			for(int i = 1; i <= num; i *= 10) {
+				cont++;
+			}
 			
-		} catch(InputMismatchException e) {
-			
+			// Mostramos un mensaje con el número de dígitos
+			System.out.println("El número " + num + " tiene " + cont + " dígitos");
+		} else {
+			// En caso de ser 0 o menor mostramos un mensaje de error
+			System.err.println("Error, número no válido");
 		}
+		
+		// Cerramos el scanner
+		sc.close();
 
 	}
 
